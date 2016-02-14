1c2a4a2ba861045285563544afdfda620d2626dc
Ravi Salruu <saluru.r@gmail.com>
2016-02-13 21:17:50 -0500

    Implementation along with the test cases.

--- src/main/java/io/egen/rentalflix/Movie.java
+++ src/main/java/io/egen/rentalflix/Movie.java
@@ -1,9 +1,93 @@
 package io.egen.rentalflix;
 
 /**
- * Entity representing a movie.
- * Fields: id, title, year, language
+ * Entity representing a movie. Fields: id, title, year, language
  */
 public class Movie {
-	//POJO IMPLEMENTATION GOES HERE
+	private Integer movieId;
+	private String name;
+	private boolean isRented;
+
+	/**
+	 * @param i
+	 * @param string
+	 * @param false1
+	 */
+	public Movie(int id, String name, Boolean notRented) {
+		this.movieId = id;
+		this.name = name;
+		this.isRented = notRented;
+	}
+
+	public Integer getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isRented() {
+		return isRented;
+	}
+
+	public void setRented(boolean isRented) {
+		this.isRented = isRented;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (isRented ? 1231 : 1237);
+		result = prime * result + ((movieId == null) ? 0 : movieId.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [movieId=" + movieId + ", name=" + name + ", isRented=" + isRented + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Movie other = (Movie) obj;
+		if (isRented != other.isRented) {
+			return false;
+		}
+		if (movieId == null) {
+			if (other.movieId != null) {
+				return false;
+			}
+		} else if (!movieId.equals(other.movieId)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
 }
--- src/main/java/io/egen/rentalflix/MovieService.java
+++ src/main/java/io/egen/rentalflix/MovieService.java
@@ -1,9 +1,96 @@
 package io.egen.rentalflix;
 
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
 /**
- * Service implementing IFlix interface
- * You can use any Java collection type to store movies
+ * Service implementing IFlix interface You can use any Java collection type to
+ * store movies
  */
 public class MovieService implements IFlix {
+
+	private static ConcurrentHashMap<Integer, Movie> movieList = new ConcurrentHashMap<Integer, Movie>();
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see io.egen.rentalflix.IFlix#findAll()
+	 */
+	@Override
+	public List<Movie> findAll() {
+		return movieList.values().stream().collect(Collectors.toList());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see io.egen.rentalflix.IFlix#findByName(java.lang.String)
+	 */
+	@Override
+	public List<Movie> findByName(String name) {
+		return movieList.values().stream().filter(movie -> movie.getName().equals(name)).collect(Collectors.toList());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see io.egen.rentalflix.IFlix#create(io.egen.rentalflix.Movie)
+	 */
+	@Override
+	public Movie create(Movie movie) {
+		movieList.put(movie.getMovieId(), movie);
+		return movie;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see io.egen.rentalflix.IFlix#update(io.egen.rentalflix.Movie)
+	 */
+	@Override
+	public Movie update(Movie movie) {
+		if (movieList.get(movie.getMovieId()) != null) {
+			movieList.put(movie.getMovieId(), movie);
+		} else {
+			throw new IllegalArgumentException("No such movie was found");
+		}
+		return movie;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see io.egen.rentalflix.IFlix#delete(int)
+	 */
+	@Override
+	public Movie delete(int id) {
+		Movie movie = movieList.get(id);
+		if (movie != null) {
+			movieList.remove(id);
+		} else {
+			throw new IllegalArgumentException("No such movie was found");
+		}
+		return movie;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see io.egen.rentalflix.IFlix#rentMovie(int, java.lang.String)
+	 */
+	@Override
+	public boolean rentMovie(int movieId, String user) {
+		Movie movie = movieList.get(movieId);
+		boolean canMovieBeRented = false;
+		if (movie.isRented()) {
+			throw new IllegalArgumentException("This movie " + movie.getName() + " has already been rented");
+		} else {
+			movie.setRented(Boolean.TRUE);
+			update(movie);
+			canMovieBeRented = Boolean.TRUE;
+		}
+		return canMovieBeRented;
+	}
 
 }
